ccb8bd046862ac5d6b55e1da9d1f7a37545e9c9d
liumm <mengmeng.liu@yizhun-ai.com>
2023-12-07 19:07:18 +0800

    feat: router lazy load demo1

--- src/App2.js
+++ src/App2.js
@@ -3,8 +3,8 @@ import { BrowserRouter, RouterProvider } from "react-router-dom";
 import BasicRouter from "./features/MyRouter/01.BasicRouter";
 import NestedRouting from "./features/MyRouter/02.NestedRouting";
 import QueryParsing from "./features/MyRouter/03.QueryParsing";
-import TabsPage from "./features/MyRouter/TabsPage";
 import AuthRouter from "./features/MyRouter/04.AuthRouter";
+import LazyloadDemo1 from "./features/MyRouter/05-1.lazyload";
 
 
 const routes = [
@@ -12,19 +12,19 @@ const routes = [
   ["02.NestedRouting", NestedRouting],
   ["03.QueryParsing", QueryParsing],
   ["04.AuthRouter", AuthRouter],
-  // ["RouterAuth", RouterAuth],
+  ["05-1.LazyloadDemo1", LazyloadDemo1 ],
 ];
 
 
 export default function App() {
   return (
-    <>
+    <div style={{padding: 30}}>
       {routes.map(([label, Component], index) => (
         <div key={index}>
           <h3 style={{borderBottom: '1px solid #000', margin: '20px 0'}}>{label}</h3>
           <Component />
         </div>
       ))}
-    </>
+    </div>
   );
 }
